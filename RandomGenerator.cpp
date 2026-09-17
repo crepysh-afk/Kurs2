ad51f78edd1147c10b53b82934d6e3d8ff09bd2b
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2026-09-17 19:12:28 +0300

    Refactor constructor parameter types in RandomGenerator

--- RandomGenerator.cpp
+++ RandomGenerator.cpp
@@ -2,7 +2,7 @@
 
 namespace miit::algebra
 {
-    RandomGenerator::RandomGenerator(int minValue, int maxValue)
+    RandomGenerator::RandomGenerator(const int minValue,const int maxValue)
         : distribution(minValue, maxValue),
           engine(std::random_device{}())
     {
