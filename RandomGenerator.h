9728326f7ca0d0a5e39f342e3b5048448f080acc
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2026-09-17 15:37:06 +0300

    Update RandomGenerator.h

--- RandomGenerator.h
+++ RandomGenerator.h
@@ -5,14 +5,32 @@
 
 namespace miit::algebra
 {
+/**
+* @brief Генератор случайных целых чисел.
+*/
     class RandomGenerator final : public Generator
     {
     private:
+/**
+* @brief Распределение случайных чисел.
+*/
         std::uniform_int_distribution<int> distribution;
+/**
+* @brief Генератор псевдослучайных чисел.
+*/
         std::mt19937 engine;
 
     public:
+/**
+* @brief Создаёт генератор случайных чисел.
+* @param minValue Минимальное значение диапазона.
+* @param maxValue Максимальное значение диапазона.
+*/
         RandomGenerator(int minValue, int maxValue);
+/**
+* @brief Генерирует случайное число.         
+* @return Случайное целое число из заданного диапазона.
+*/
         int generate() override;
     };
 }
