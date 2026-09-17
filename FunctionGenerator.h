271ab0a5093d351681bd77757bfe37bf08a567e9
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2026-09-17 19:10:24 +0300

    Update FunctionGenerator.h

--- FunctionGenerator.h
+++ FunctionGenerator.h
@@ -5,15 +5,29 @@
 
 namespace miit::algebra
 {
-    // Универсальный генератор. В main можно передать функцию,
-    // которая читает значение с клавиатуры или возвращает число другим способом.
+    /**
+     * @brief Генератор значений с использованием пользовательской функции.
+     *
+     * Позволяет передать любой алгоритм получения целого числа, например
+     * функцию чтения значения с клавиатуры.
+     */
     class FunctionGenerator final : public Generator
     {
     private:
         std::function<int()> function;
 
     public:
-        explicit FunctionGenerator(std::function<int()> function);
+        /**
+         * @brief Создаёт генератор на основе переданной функции.
+         * @param function Функция, возвращающая очередное целое число.
+         * @throw std::invalid_argument Если передана пустая функция.
+         */
+        explicit FunctionGenerator(const std::function<int()> function);
+
+        /**
+         * @brief Получает очередное значение вызовом сохранённой функции.
+         * @return Целое число, возвращённое пользовательской функцией.
+         */
         int generate() override;
     };
 }
