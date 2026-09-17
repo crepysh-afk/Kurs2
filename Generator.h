f3a91f587807934ab3d46495a82612197e8e5b1e
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2026-09-17 15:34:25 +0300

    Update Generator.h

--- Generator.h
+++ Generator.h
@@ -2,10 +2,20 @@
 
 namespace miit::algebra
 {
+/**
+* @brief Абстрактный базовый класс генератора чисел.
+*/
     class Generator
     {
     public:
+/**
+* @brief Виртуальный деструктор.
+*/
         virtual ~Generator() = default;
+/**
+* @brief Генерирует одно целое число.
+* @return Сгенерированное целое число.
+*/
         virtual int generate() = 0;
     };
 }
